1d77518ff67c2b819f4e2db698e087379b85b1a6
imri <imri.paran@mail.huji.ac.il>
2020-02-18 18:00:21 +0200

    api sanity test

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,29 @@
+import os
+from dotenv import load_dotenv
+import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from app.models.cash_flow import CashFlow
-from app.models.cash_flow_mapping import GeneralCashFlowMapping
+load_dotenv()
 
 
 def test_join():
     engine = create_engine("mysql://root:1234@localhost:3306/finance")
     Session = sessionmaker(bind=engine)
     session = Session()
-    print(session.query(CashFlow).outerjoin(GeneralCashFlowMapping, CashFlow.name == GeneralCashFlowMapping.name).all())
+    print(
+        session.query(CashFlow)
+        .outerjoin(GeneralCashFlowMapping, CashFlow.name == GeneralCashFlowMapping.name)
+        .all()
+    )
+
+
+def test_firefly_api():
+    res = requests.get(
+        os.environ["ENDPOINT"] + "/api/v1/about",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + os.environ["TOKEN"],
+        },
+    )
+    assert "login" not in res.url
